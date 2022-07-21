c363d54c84c5893071eda040416ebcc2a1d134da
Sagar Goyal <sagar.goyal@stackpro.in>
2022-07-21 12:19:53 +0530

    created all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarNavComponent } from './sidebar-nav/sidebar-nav.component';
+import { CartComponent } from './cart/cart.component';
+import { OrdersComponent } from './orders/orders.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarNavComponent,
+    CartComponent,
+    OrdersComponent,
+    CheckoutComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule
