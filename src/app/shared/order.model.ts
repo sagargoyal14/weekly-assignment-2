9d7d3be286a570f45293e67742c6f60dc8e7513a
Sagar Goyal <sagar.goyal@stackpro.in>
2022-07-22 01:28:31 +0530

    added status field in order model

--- src/app/shared/order.model.ts
+++ src/app/shared/order.model.ts
@@ -2,5 +2,5 @@ import { Cart } from "./cart.model";
 
 
 export class Order{
-    constructor(public orders:Cart[], public quantity:number){}
+    constructor(public orders:Cart[], public quantity:number, public price:number, public status:string) {}
 }
